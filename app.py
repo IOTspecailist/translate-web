2d95e770113fceb98166754baf720d15cf2f220f
nobody <asdf@asdffasdassadsad.com>
2025-06-21 11:58:19 +0900

    feat:번역기 UI와 서버 구현

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from googletrans import Translator
+
+app = Flask(__name__)
+translator = Translator()
+
+@app.route('/')
+def index():
+    with open("index.html", "r", encoding="utf-8") as f:
+        return f.read()
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    data = request.get_json()
+    text = data.get('text', '')
+    translated = translator.translate(text, src='ko', dest='en')
+    return jsonify({'translated': translated.text})
+
+if __name__ == '__main__':
+    app.run(debug=True)
